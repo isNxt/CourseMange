6a6bca6a1f4020d4f9868a068e4de5b0016285a2
isNxt <36531399+isNxt@users.noreply.github.com>
2018-12-15 00:02:02 +0800

    finfish teacher side course related pages

--- Mobile/assets/js/common.js
+++ Mobile/assets/js/common.js
@@ -40,7 +40,11 @@ function createUser() {
     alert("success");
     return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
 }
-
+function createClass() {
+// jquery 表单提交
+    alert("success");
+    return true; // 必须返回false，否则表单会自己再做一次提交操作，并且页面跳转
+}
 function createCourse() {
 // jquery 表单提交
     alert("success");
